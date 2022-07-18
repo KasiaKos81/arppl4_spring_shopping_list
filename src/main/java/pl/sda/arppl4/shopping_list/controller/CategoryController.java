8780fc719faa85de58a509e13c9afbd1f3aac5ea
Kasia <kasiakos81@gmail.com>
2022-07-18 21:27:05 +0200

    180722

--- src/main/java/pl/sda/arppl4/shopping_list/controller/CategoryController.java
+++ src/main/java/pl/sda/arppl4/shopping_list/controller/CategoryController.java
@@ -2,9 +2,13 @@ package pl.sda.arppl4.shopping_list.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import pl.sda.arppl4.shopping_list.model.Category;
+import pl.sda.arppl4.shopping_list.model.dto.CategoryDto;
 import pl.sda.arppl4.shopping_list.service.CategoryService;
+
+import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -14,6 +18,30 @@ import pl.sda.arppl4.shopping_list.service.CategoryService;
 public class CategoryController {
 
     private final CategoryService categoryService;
+
+    @GetMapping("/getAll")
+    public List<CategoryDto> getAllCategories() {
+        log.info("This method displays list of all categories");
+        List<CategoryDto> listOfAllCategories = categoryService.getAllCategories();
+        return listOfAllCategories;
+    }
+
+    @DeleteMapping("/deleteCategory{identifier}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCategory(@PathVariable(name="identifier") Long identyfikator){
+        log.info("This method allows to delete a category");
+        categoryService.deleteById(identyfikator);
+    }
+
+    @PostMapping("/addCategory")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addCategory(@RequestBody Category category, String name){
+        log.info("This method allows to add category");
+        categoryService.addCategory(category, name);
+
+    }
+
+
 
 
 }
--- src/main/java/pl/sda/arppl4/shopping_list/service/CategoryService.java
+++ src/main/java/pl/sda/arppl4/shopping_list/service/CategoryService.java
@@ -3,7 +3,15 @@ package pl.sda.arppl4.shopping_list.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import pl.sda.arppl4.shopping_list.model.Category;
+import pl.sda.arppl4.shopping_list.model.Product;
+import pl.sda.arppl4.shopping_list.model.dto.CategoryDto;
 import pl.sda.arppl4.shopping_list.repository.CategoryRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 @Slf4j
 @Service
@@ -12,4 +20,38 @@ import pl.sda.arppl4.shopping_list.repository.CategoryRepository;
 public class CategoryService {
 
     private final CategoryRepository categoryRepository;
+
+    public List<CategoryDto> getAllCategories() {
+        List<Category> categoryList = categoryRepository.findAll();
+
+        List<CategoryDto> categoryDtoList = new ArrayList<>();
+        for (Category category : categoryList) {
+            categoryDtoList.add(category.mapToCategoryDto());
+        }
+        return categoryDtoList;
+
+    }
+
+    public void addCategory(Category category, String name) {
+
+    }
+
+    public void deleteById(Long identyfikator) {
+        Optional<Category> optionalCategory = categoryRepository.findById(identyfikator);
+        if(optionalCategory.isPresent()){
+         Category kategoriaDoDelete = optionalCategory.get();
+            Set<Product> setOfProducts = optionalCategory.get().getProductSet();
+            if(!setOfProducts.isEmpty()) {
+                System.out.println("This category cannot be deleted");
+            } else {
+                categoryRepository.deleteById(identyfikator);
+                System.out.println("Category is deleted from database");
+            }
+
+            } else {
+            System.out.println("No such category");
+        }
+
+
+    }
 }
